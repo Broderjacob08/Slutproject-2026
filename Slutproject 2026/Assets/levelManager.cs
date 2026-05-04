cf4f5ad574f874f2d708620eedda4f49d9216ec6
Broderjacob08 <alexander.hammarlund@elev.ga.lbs.se>
2026-05-04 13:41:33 +0200

    Boom ny repository

---
+++ Slutproject 2026/Assets/levelManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class levelManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public static levelManager instance;
+    public string LevelName;
+    void Start()
+    {
+        if(instance = null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(LevelName);
+    }
+}
