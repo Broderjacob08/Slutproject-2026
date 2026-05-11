7a64788d08a9d5b9d509923f0c3f58043d2dfdaa
Broderjacob08 <alexander.hammarlund@elev.ga.lbs.se>
2026-05-11 13:45:28 +0200

    började på HP counter

--- Slutproject 2026/Assets/Scripts/Unit_Stats.cs
+++ Slutproject 2026/Assets/Scripts/Unit_Stats.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 //Alexander
 public class Unit_Stats : MonoBehaviour
@@ -7,9 +8,19 @@ public class Unit_Stats : MonoBehaviour
     public int currentHP;
     public int damage;
 
+    public TextMeshProUGUI HPCounter;
+    private void Start()
+    {
+        HPCounter = GetComponent<TextMeshProUGUI>();
+    }
+
     public void TakeDamage(int amount)
     {
         currentHP -= amount;
         if (currentHP < 0) currentHP = 0;
+    }
+    private void Update()
+    {
+        HPCounter.text = currentHP + "of" + maxHP;
     }
 }
