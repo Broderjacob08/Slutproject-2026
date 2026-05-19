02904c3187494e7825f44db3fd26268c88ffacc2
i-j-p-g <ian.jimenezpradosberg@elev.ga.lbs.se>
2026-05-19 11:08:15 +0200

    Updated levelmanager

--- Slutproject 2026/Assets/Scripts/levelManager.cs
+++ Slutproject 2026/Assets/Scripts/levelManager.cs
@@ -1,31 +1,41 @@
 using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
-//Alexander
+//Ian
 
 [System.Serializable]
 public class levelManager : MonoBehaviour
 {
-    //Ian
-
+    
     //public static levelManager instance;
     //public string LevelName;
-    public List<levelConnect> ConnectedLevels = new List<levelConnect>();
 
     /*public void OnClick()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(LevelName);
     }*/
+
+    public List<level> Levels = new List<level>();
+
+    public void UnlockLevels()
+    {
+        // Loop igenom varje level och jämföra keygiven med keyneeded
+
+        
+    }
 }
-
-
 [System.Serializable]
-public class levelConnect
+public class level
 {
-    public List<int> ConnectedLevels = new List<int>();
+    public int keyNeeded;
+    public int keyGiven;
+    public GameObject blah;
+    public bool isLocked;
+
+    
 }
 
-public class Level
+public class LevelLock //check if the level is locked or not
 {
 
 }
