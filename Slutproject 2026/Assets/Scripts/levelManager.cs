ef1c87f6b4e349d8e2096e6e692b9a6c980cf88c
i-j-p-g <ian.jimenezpradosberg@elev.ga.lbs.se>
2026-05-20 14:17:07 +0200

    Uppdaterade level manager

--- Slutproject 2026/Assets/Scripts/levelManager.cs
+++ Slutproject 2026/Assets/Scripts/levelManager.cs
@@ -1,5 +1,7 @@
+using JetBrains.Annotations;
 using NUnit.Framework;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 //Ian
 
@@ -7,35 +9,45 @@ using UnityEngine;
 public class levelManager : MonoBehaviour
 {
     
-    //public static levelManager instance;
-    //public string LevelName;
-
-    /*public void OnClick()
-    {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(LevelName);
-    }*/
+    public static levelManager instance;
+    public string LevelName;
 
     public List<level> Levels = new List<level>();
+
+    public string keyneeded;
+    public string keygiven;
+    public bool isLocked;
+
+}
+[System.Serializable]
+public class level
+{
+    public int keyneeded;
+    public int keygiven;
+    public GameObject blah;
+    public bool isLocked;
+    public string LevelName;
 
     public void UnlockLevels()
     {
         // Loop igenom varje level och jämföra keygiven med keyneeded
 
-        
+        if (keygiven == keyneeded)
+        {
+            isLocked = false;
+        }
     }
-}
-[System.Serializable]
-public class level
-{
-    public int keyNeeded;
-    public int keyGiven;
-    public GameObject blah;
-    public bool isLocked;
 
-    
-}
+    public void OnClick()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(LevelName);
+    }
 
-public class LevelLock //check if the level is locked or not
-{
-
+    public void LevelLock() //kollar om leveln är låst
+    {
+        if (isLocked == false)
+        {
+            OnClick();
+        }
+    }
 }
